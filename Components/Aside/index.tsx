a3c4c3ac1419cbc0bf329ab9a463ea7dd7058459
Dev-Nadson <nadson.pros@gmail.com>
2025-12-03 14:40:09 -0300

    Feat: Modularização

--- Components/Aside/index.tsx
+++ Components/Aside/index.tsx
@@ -1,17 +1,1 @@
-import Image from "next/image"
-import Link from "next/link"
-
-function AsideToolBar() {
-    return (
-        <aside>
-            <div>
-                <span>
-                    <Image src="../../../prescription.svg" width={100} height={100} alt="Logo da liga" />
-                    <Link href="#">LAFAC GERENCIAMENTO</Link>
-                </span>
-            </div>
-        </aside>
-    )
-}
-
-export { AsideToolBar }
+export * from "./Aside"
